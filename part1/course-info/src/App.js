be2ca3c891965a3ef01bda39688d03eb6e66a798
Sahil Singh <sahil.dogstar@gmail.com>
2021-09-13 10:15:57 +0530

    test

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -7,7 +7,7 @@ import React from 'react'
 const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+  const part2 = 'Using props to pass datad'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
